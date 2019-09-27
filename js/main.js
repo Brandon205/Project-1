10ff5bb5040ca3b97b05fd12369b37613219d1ac
Brandon205 <brandonblack02@gmail.com>
2019-09-27 16:45:57 -0700

    D1 - Basic square movement made

--- js/main.js
+++ js/main.js
@@ -4,19 +4,37 @@ var ctx = canvas.getContext('2d');
 console.log("hie")
 //Game Pieces 
 var snake = {
-    length: [],
+    tail: [],
     x: 20,
     y: 200,
+    long: 0,
+    mvtX: 0,
+    mvtY: 0,
 }
 
 
-
 //Functions
-function movement(evt) {//38 39 40 37
+
+var going = function(x, y) {
+    snake.mvtX = x * 10;
+    snake.mvtY = y * 10;
+}
+
+function movement(evt) {//87 65 83 68
     console.log(evt.keyCode);
     switch (evt.keyCode) {
-        case (37):
-            setDir(left);
+        case (87): //up
+            going(0, -1);
+            break;
+        case (68): //right
+            going(1, 0);
+            break;
+        case (83): //bottom
+            going(0, 1);
+            break; 
+        case (65): //left
+            going(-1, 0);
+            break;
     }
 }
 
@@ -26,6 +44,8 @@ function gameLoop() {
     
     ctx.fillStyle = "white";
     ctx.fillRect(snake.x, snake.y, 20, 20);
+    snake.x += snake.mvtX;
+    snake.y += snake.mvtY;
 }
 
 //Event Handlers 
