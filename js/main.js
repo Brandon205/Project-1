ce02b30b4a73b5064a0eb73e51daa49a48a32ba9
Brandon205 <brandonblack02@gmail.com>
2019-09-28 13:41:56 -0700

    D2 - Basic eat function working

--- js/main.js
+++ js/main.js
@@ -5,45 +5,43 @@ var ctx = canvas.getContext('2d');
 //Game Pieces 
 var snake = {
     tail: [],
-    x: 20,
-    y: 200,
     long: 1,
+    x: 20,
+    y: 180,
     mvtX: 0,
     mvtY: 0,
 }
 
 var apple = {
-    x: Math.floor(Math.random() *380),
-    y: Math.floor(Math.random() *380),
+    x: Math.floor(Math.random() * 10) * 40,
+    y: Math.floor(Math.random() * 10) * 40,
     newApple: function() {
-        x = Math.floor(Math.random() *380);
-        y = Math.floor(Math.random() *380);
+        apple.x = Math.floor(Math.random() * 10) * 40;
+        apple.y = Math.floor(Math.random() * 10) * 40;
     }
 }
+
 //Functions
 
-function appleLoc() {
-    let x = Math.floor(Math.random() * 380);
-    let y = Math.floor(Math.random() * 380);
-    console.log(x);
-    ctx.fillStyle = "red";
-    food = ctx.fillRect(x, y, 20, 20);
+function move() {
+    for (let i = 0; i < snake.tail.length; i++) {
+        ctx.fillStyle = "grey";
+        ctx.fillRect(snake.tail[i].x, snake.tail[i].y, 20, 20);
+    }
 }
-appleLoc();
-console.log(food);
-var going = function(x, y) {
-    snake.mvtX = x * 10;
-    snake.mvtY = y * 10;
+
+function grow() {
+    snake.tail.push({x: snake.x, y: snake.y});
+    snake.long++;
 }
-// function wallDetection() {
-//     if (snake.x >= canvas.width) {
-//         //TODO: ADD gameOver() here to end the game 
-//         snake.mvtX = 0;
-//         console.log('the if ')
-//     } else {
-//         console.log('else')
-//     }
-// }
+
+var chomp = function() {
+    if (snake.x === apple.x && snake.y === apple.y) {
+    return true;
+    } else {
+        return false;
+    }
+}
 
 function movement(evt) {//87 65 83 68
     switch (evt.keyCode) {
@@ -62,21 +60,58 @@ function movement(evt) {//87 65 83 68
     }
 }
 
+var going = function(x, y) {
+    snake.mvtX = x * 20;
+    snake.mvtY = y * 20;
+}
+
+function gameOver() {
+    if (snake.x > canvas.width - 20) {
+        snake.x = -20;
+    }
+    if (snake.x < 0) {
+        snake.x = canvas.width;
+    }
+    if (snake.y > canvas.height + 20) {
+        snake.y = 0;
+    }
+    if (snake.y < 0) {
+        snake.y = canvas.height;
+    }
+}
+
 function gameLoop() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);//drawing canvas 
     
-    ctx.fillStyle = "white";
-    ctx.fillRect(snake.x, snake.y, 20, 20);//Drawing snake
     snake.x += snake.mvtX;
     snake.y += snake.mvtY;
-    
+    move();
+    snake.tail.push({x: snake.x, y: snake.y});
+    if (snake.tail.length > snake.long) {
+        snake.tail.shift();
+    }
+    gameOver();
+
     ctx.fillStyle = 'red';
     ctx.fillRect(apple.x, apple.y, 20, 20);
+
+    if (chomp()) {
+        grow();
+        apple.newApple();
+    }
+
 }
 
 //Event Handlers 
 document.addEventListener('keydown', function(e) {
     movement(e);
 })
-setInterval(gameLoop, 1000/10);
+
+// document.addEventListener('keydown', function(e) {
+//     if (e.keyCode === " ") {
+//         console.log('space');
+//         grow();
+//     }
+// })
+setInterval(gameLoop, 1000/8);
